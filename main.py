314791a4eb63ba56f4c2c844a7f63966a878f9f0
rootrc <138728071+rootrc@users.noreply.github.com>
2025-10-01 16:29:41 -0400

    numpy vectorization to speed up program

--- main.py
+++ main.py
@@ -10,29 +10,33 @@ octaves = 6
 persistence = 0.4
 lacunarity = 2.0
 
-def generate_continent(width, height, scale=100, seed=0, cx=width/2, cy=height / 2):
+def generate_continent(width, height, scale, seed, cx = width/2, cy = height/2):
     heightmap = np.zeros((height, width))
-    nx_scale = np.random.randint(30, 75) / 50
+    nx_scale = np.random.randint(25, 100) / 50
 
-    
-    for y in range(height):
-        for x in range(width):
-            nx = (x - cx) / scale
-            ny = (y - cy) / scale
-            distort_noise = snoise2(nx*2, ny*2, octaves=2, base=seed + 100)
-            nx_distorted = nx * nx_scale + distort_noise * 0.3
-            ny_distorted = ny + distort_noise * 0.3
-            distance = (np.sqrt(nx_distorted**2 + ny_distorted**2) / 2.2) ** 1.2
-            
-            noise_val = snoise2(nx, ny,
-                                octaves=octaves,
-                                persistence=persistence,
-                                lacunarity=lacunarity,
-                                repeatx=width,
-                                repeaty=height, 
-                                base=seed)
-            noise_val = (noise_val + 1) / 2
-            heightmap[y][x] = noise_val - distance
+    x = np.arange(width)
+    y = np.arange(height)
+    xx, yy = np.meshgrid(x, y)
+
+    nx = (xx - cx) / scale
+    ny = (yy - cy) / scale
+
+    distort_noise = np.vectorize(lambda x, y: snoise2(x * 2, y * 2, octaves = 2, base = seed + 100))(nx, ny) * 0.3
+    nx_distorted = nx * nx_scale + distort_noise
+    ny_distorted = ny + distort_noise
+
+    distance = (np.sqrt(nx_distorted**2 + ny_distorted**2) / 2.2) ** 1.2
+
+    noise_val = np.vectorize(lambda x, y: snoise2(x, y,
+                                                  octaves = octaves,
+                                                  persistence = persistence,
+                                                  lacunarity = lacunarity,
+                                                  repeatx = width,
+                                                  repeaty = height,
+                                                  base = seed))(nx, ny)
+
+    noise_val = (noise_val + 1) / 2
+    heightmap = noise_val - distance
     heightmap = np.clip(heightmap, 0, 1)
     return heightmap
 
@@ -47,8 +51,8 @@ for i in range(num_continents):
     continent = generate_continent(width, height, scale, seed, cx, cy)
     global_map = np.add(global_map, continent)
 
-plt.figure(figsize=(8, 8))
-plt.imshow(global_map, cmap='terrain')
+plt.figure(figsize = (8, 8))
+plt.imshow(global_map, cmap = 'terrain')
 plt.title("Simplex Noise - Continental Shape")
 plt.axis('off')
 plt.tight_layout()
