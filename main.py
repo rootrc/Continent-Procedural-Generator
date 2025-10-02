c63ac4c4006c35310b98204fc112af88d20ba4b3
rootrc <138728071+rootrc@users.noreply.github.com>
2025-10-02 15:14:27 -0400

    adjusted noise generation

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from noise import snoise2
 
-width = 512
-height = 512
+global_width = 512
+global_height = 512
 
 octaves = 6
-persistence = 0.4
+persistence = 0.5
 lacunarity = 2.0
 
-def generate_continent(width, height, scale, seed, cx = width/2, cy = height/2):
+def generate_continent(width, height, scale, seed, cx = global_width/2, cy = global_height/2):
     heightmap = np.zeros((height, width))
-    nx_scale = np.random.randint(25, 100) / 50
+    nx_scale = np.random.uniform(0.8, 1.2)
 
     x = np.arange(width)
     y = np.arange(height)
@@ -21,11 +21,10 @@ def generate_continent(width, height, scale, seed, cx = width/2, cy = height/2):
     nx = (xx - cx) / scale
     ny = (yy - cy) / scale
 
-    distort_noise = np.vectorize(lambda x, y: snoise2(x * 2, y * 2, octaves = 2, base = seed + 100))(nx, ny) * 0.3
+    distort_noise = np.vectorize(lambda x, y: snoise2(x * 2, y * 2, octaves = 3, base = seed + 100))(nx, ny) * 0.3
     nx_distorted = nx * nx_scale + distort_noise
     ny_distorted = ny + distort_noise
-
-    distance = (np.sqrt(nx_distorted**2 + ny_distorted**2) / 2.2) ** 1.2
+    
 
     noise_val = np.vectorize(lambda x, y: snoise2(x, y,
                                                   octaves = octaves,
@@ -36,20 +35,24 @@ def generate_continent(width, height, scale, seed, cx = width/2, cy = height/2):
                                                   base = seed))(nx, ny)
 
     noise_val = (noise_val + 1) / 2
+    distance = (np.sqrt(nx_distorted**2 + ny_distorted**2) / 2.2) ** 1.3
     heightmap = noise_val - distance
+
     heightmap = np.clip(heightmap, 0, 1)
     return heightmap
 
 num_continents = 5
 
-global_map = np.zeros((height, width))
+global_map = np.zeros((global_height, global_width))
 
 for i in range(num_continents):
     seed = np.random.randint(1, 1000)
-    cx, cy = np.random.randint(10, 90) / 100 * width, np.random.randint(10, 90) / 100 * height
+    cx, cy = np.random.uniform(0.1, 0.9) * global_width, np.random.uniform(0.1, 0.9) * global_height
     scale = np.random.randint(50, 100)
-    continent = generate_continent(width, height, scale, seed, cx, cy)
-    global_map = np.add(global_map, continent)
+    continent = generate_continent(global_width, global_height, scale, seed, cx, cy)
+    global_map += continent
+    
+global_map = np.clip(global_map, 0, 1)
 
 plt.figure(figsize = (8, 8))
 plt.imshow(global_map, cmap = 'terrain')
